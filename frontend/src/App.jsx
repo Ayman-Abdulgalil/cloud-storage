f8d1df8ae987cb0a48e8e89a3eaa1d896a0eab98
Victory Imadi <omojoimadi711@gmail.com>
2026-01-20 16:12:17 +0530

    Login Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,13 @@
 // import { useState } from 'react'
-// import { Container, Typography, Button, Box, Card, CardContent } from "@mui/material";
+
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 
-import './App.css'
-
 function App() {
   // const [count, setCount] = useState(0)
-
   return (
-    <>
       <Login />
-    </>
-  )
+  );
 }
 
 export default App
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,65 @@
-function Login(){
-    return <h1>Login Page</h1>
+import { Box, Card, TextField, Button, Typography } from "@mui/material";
+import Logo from "../assets/logo.svg";
+
+
+function Login() {
+    return (
+        <Card 
+        sx={{
+            width: 380, 
+            padding: 7, 
+            borderRadius: 3, 
+            backgroundColor: "#ffffff",
+            boxShadow: "0px 10px 40px rgba(0,0,0,0.3)", 
+            textAlign: "center"
+        }}>
+
+        {/* Title */}
+        <Box display="flex" alignItems="center" flexDirection="column" mb={2}>
+
+            <img src={Logo} alt="Secure Drive logo" style={{ width: 150, height: 150, marginBottom: 6 }} />
+
+            <Typography variant="h3" fontWeight={700} lineHeight={1.1} sx={{ whiteSpace: "nowrap", fontSize: "1.8rem" }}>
+                Secure Drive
+            </Typography>
+        </Box>
+
+
+        {/* Email */}
+        <Box mb={2} textAlign="left">
+            <Typography variant="body2" mb={0.5}>
+                Email
+            </Typography>
+            <TextField fullWidth size="small" placeholder="Enter your email" />
+        </Box>
+
+        {/* Password */}
+        <Box mb={2} textAlign="left">
+            <Typography variant="body2" mb={0.5}>
+                Password
+            </Typography>
+            <TextField fullWidth size="small" type="password" placeholder="Enter your password" />
+        </Box>
+
+        {/* Forgot password */}
+        <Typography variant="body2" sx={{ 
+            textAlign: "right", 
+            mb: 2, 
+            cursor: "pointer" }}>
+                Forgot password?
+        </Typography>
+
+        {/* Login button */}
+        <Button fullWidth variant="contained" sx={{
+            backgroundColor: "#4F46E5", 
+            borderRadius: 2, 
+            textTransform: "none", 
+            fontWeight: 500, 
+            py: 1, mb: 2}}>
+                Login
+        </Button>
+        </Card>
+    );
 }
 
 export default Login;
